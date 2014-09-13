9b5a847babbc62030fe85b69562a756676e81f82
David Kitchener <dkitchener@davids-mbp.att.net>
2014-09-12 22:59:44 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuneSearch::Application.config.secret_token = '841074f6cdd4247d70889c7029bcb29265f9547bf6797c6dcf9c58efe60d927ebf7b89ee4b6632ccefc82d94e307c7f3957d75dde15533f3000f55e413ae973d'
